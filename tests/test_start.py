7cb8429fe020236cec34b36b9b3e491e001d91f0
Emiel Zyde <emielzyde1@hotmail.com>
2019-07-19 10:23:35 +0100

    Deleted test files

--- tests/test_start.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-from msq_grammar_correction import start
-
-
-class StartTest(TestCase):
-    def test_start(self):
-        self.assertIsInstance(start.config, dict)
